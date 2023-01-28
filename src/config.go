4c436959214626428a2a94ab6ff7ce3ea8d2a5c8
João Pedro Limão <johnazedo07@gmail.com>
2023-01-28 15:43:45 -0300

    Update README.md

--- src/config.go
+++ src/config.go
@@ -19,7 +19,7 @@ type Config struct {
 }
 
 var DefaultConfig = &Config{
-	GPTModel:          "text-davinci-003",
+	GPTModel:          "text-babbage-001",
 	GPTTemperature:    0.9,
 	GPTMaxTokens:      256,
 	GPTCompletionsUrl: "https://api.openai.com/v1/completions",
